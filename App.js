a10d3dc74ba4a7713230e839b579ed46666e72c2
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-06-05 05:06:42 +0600

    learned about Imgae, ScrollView and Pressable

--- App.js
+++ App.js
@@ -1,29 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  Pressable,
+} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <View style={{ height: 50, backgroundColor: 'blue' }}>
-        <Text>This is a text</Text>
-      </View>
-      <View style={{ height: 50, backgroundColor: 'green' }}>
-        <Text
-          style={{
-            color: 'white',
-            padding: 10,
-            fontWeight: 'bold',
-            fontSize: 24,
+      <ScrollView>
+        <View style={{ height: 50, backgroundColor: 'blue' }}>
+          <Text>This is a text</Text>
+        </View>
+        <View style={{ height: 50, backgroundColor: 'green' }}>
+          <Text
+            style={{
+              color: 'white',
+              padding: 10,
+              fontWeight: 'bold',
+              fontSize: 24,
+            }}
+          >
+            This is another text
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 50, marginLeft: 40 }}>
+          <Image
+            style={{ height: 180, width: 200 }}
+            source={require('./assets/favicon.png')}
+            resizeMode="container"
+          />
+        </View>
+        <View style={{ marginTop: 50, marginLeft: 40 }}>
+          <Image
+            style={{ height: 180, width: 200 }}
+            source={{
+              uri: 'https://avatars.githubusercontent.com/u/61866994?v=4',
+            }}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={{ marginTop: 50, marginLeft: 40 }}>
+          <Image
+            style={{ height: 180, width: 200 }}
+            source={{
+              uri: 'https://avatars.githubusercontent.com/u/61866994?v=4',
+            }}
+            resizeMode="cover"
+          />
+        </View>
+        <Pressable
+          onPress={() => {
+            alert('Pressed');
           }}
         >
-          This is another text
-        </Text>
-      </View>
-
-      <Image
-        style={{ height: 80, width: 120 }}
-        source={require('./assets/favicon.png')}
-      />
+          <View
+            style={{ height: 60, width: 60, backgroundColor: 'red' }}
+          ></View>
+        </Pressable>
+        <View>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            quisquam molestiae, itaque maxime vel illo reiciendis aliquid culpa!
+            Repellat tempora nesciunt dolorem ad tenetur id laborum est
+            laudantium iste neque error fugit possimus in incidunt, magnam
+            mollitia expedita quaerat vero!
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
