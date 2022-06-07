f62203e887aa86d6635e48ac63d3e42c6f8fea7c
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-06-07 18:35:33 +0600

    learned how to add style

--- App.js
+++ App.js
@@ -27,78 +27,104 @@ export const Button = ({ title }) => {
   );
 };
 
+// export default function App() {
+//   return (
+//     <SafeAreaView>
+//       <ScrollView>
+//         <View style={{ height: 50, backgroundColor: 'blue' }}>
+//           <Text>This is a text</Text>
+//         </View>
+//         <View style={{ height: 50, backgroundColor: 'green' }}>
+//           <Text
+//             style={{
+//               color: 'white',
+//               padding: 10,
+//               fontWeight: 'bold',
+//               fontSize: 24,
+//             }}
+//           >
+//             This is another text
+//           </Text>
+//         </View>
+
+//         <View style={{ marginTop: 50, marginLeft: 40 }}>
+//           <Image
+//             style={{ height: 180, width: 200 }}
+//             source={require('./assets/favicon.png')}
+//             resizeMode="container"
+//           />
+//         </View>
+//         <View style={{ marginTop: 50, marginLeft: 40 }}>
+//           <Image
+//             style={{ height: 180, width: 200 }}
+//             source={{
+//               uri: 'https://avatars.githubusercontent.com/u/61866994?v=4',
+//             }}
+//             resizeMode="cover"
+//           />
+//         </View>
+//         <View style={{ marginTop: 50, marginLeft: 40 }}>
+//           <Image
+//             style={{ height: 180, width: 200 }}
+//             source={{
+//               uri: 'https://avatars.githubusercontent.com/u/61866994?v=4',
+//             }}
+//             resizeMode="cover"
+//           />
+//         </View>
+
+//         <Button title="press"></Button>
+//         <Button title="2nd-button"></Button>
+
+//         <Pressable
+//           onPress={() => {
+//             alert('Pressed');
+//           }}
+//         >
+//           <View
+//             style={{ height: 60, width: 60, backgroundColor: 'red' }}
+//           ></View>
+//         </Pressable>
+
+//         <View>
+//           <Text>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+//             quisquam molestiae, itaque maxime vel illo reiciendis aliquid culpa!
+//             Repellat tempora nesciunt dolorem ad tenetur id laborum est
+//             laudantium iste neque error fugit possimus in incidunt, magnam
+//             mollitia expedita quaerat vero!
+//           </Text>
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
+
 export default function App() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={{ height: 50, backgroundColor: 'blue' }}>
-          <Text>This is a text</Text>
-        </View>
-        <View style={{ height: 50, backgroundColor: 'green' }}>
-          <Text
-            style={{
-              color: 'white',
-              padding: 10,
-              fontWeight: 'bold',
-              fontSize: 24,
-            }}
-          >
-            This is another text
-          </Text>
-        </View>
-
-        <View style={{ marginTop: 50, marginLeft: 40 }}>
-          <Image
-            style={{ height: 180, width: 200 }}
-            source={require('./assets/favicon.png')}
-            resizeMode="container"
-          />
-        </View>
-        <View style={{ marginTop: 50, marginLeft: 40 }}>
-          <Image
-            style={{ height: 180, width: 200 }}
-            source={{
-              uri: 'https://avatars.githubusercontent.com/u/61866994?v=4',
-            }}
-            resizeMode="cover"
-          />
-        </View>
-        <View style={{ marginTop: 50, marginLeft: 40 }}>
-          <Image
-            style={{ height: 180, width: 200 }}
-            source={{
-              uri: 'https://avatars.githubusercontent.com/u/61866994?v=4',
-            }}
-            resizeMode="cover"
-          />
-        </View>
-
-        <Button title="press"></Button>
-        <Button title="2nd-button"></Button>
-
-        <Pressable
-          onPress={() => {
-            alert('Pressed');
-          }}
-        >
-          <View
-            style={{ height: 60, width: 60, backgroundColor: 'red' }}
-          ></View>
-        </Pressable>
-
-        <View>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-            quisquam molestiae, itaque maxime vel illo reiciendis aliquid culpa!
-            Repellat tempora nesciunt dolorem ad tenetur id laborum est
-            laudantium iste neque error fugit possimus in incidunt, magnam
-            mollitia expedita quaerat vero!
-          </Text>
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.view1} />
+      <View style={styles.view2} />
+      <View style={styles.view3} />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  view1: {
+    backgroundColor: 'red',
+    flex: 1,
+  },
+  view2: {
+    backgroundColor: 'blue',
+    flex: 1,
+  },
+  view3: {
+    backgroundColor: 'green',
+    flex: 1,
+  },
+});
 
 // const styles = StyleSheet.create({
 //   container: {
